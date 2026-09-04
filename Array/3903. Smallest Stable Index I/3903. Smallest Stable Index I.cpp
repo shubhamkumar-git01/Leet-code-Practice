d1cf767479e6e7b24ffcc27da36fc13987770056
Shubham Kumar <shubham.kumar.cse27@gmail.com>
2026-09-04 11:14:04 +0530

    CodePusher: Solution for 3903. Smallest Stable Index I

---
+++ Array/3903. Smallest Stable Index I/3903. Smallest Stable Index I.cpp
@@ -0,0 +1,29 @@
+// Problem: 3903. Smallest Stable Index I
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 31.2 MB (Beats 56.73%)
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> suffixMin(n);
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(nums[i], suffixMin[i + 1]);
+        }
+
+        int prefixMax = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            prefixMax = max(prefixMax, nums[i]);
+
+            if ((long long)prefixMax - suffixMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
