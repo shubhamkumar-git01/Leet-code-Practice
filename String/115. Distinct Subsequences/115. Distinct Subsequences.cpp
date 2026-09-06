d3347583aed94b1e2dc2b9a6b05e4bc6c5663d10
Shubham Kumar <shubham.kumar.cse27@gmail.com>
2026-09-06 19:34:28 +0530

    CodePusher: Solution for 115. Distinct Subsequences

---
+++ String/115. Distinct Subsequences/115. Distinct Subsequences.cpp
@@ -0,0 +1,32 @@
+// Problem: 115. Distinct Subsequences
+// Runtime: 35 ms (Beats 34.20%)
+// Memory: 44.2 MB (Beats 38.48%)
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                dp[i][j] = dp[i - 1][j];
+
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] += dp[i - 1][j - 1];
+
+                    if (dp[i][j] > INT_MAX)
+                        dp[i][j] = INT_MAX;
+                }
+            }
+        }
+
+        return (int)dp[n][m];
+    }
+};
